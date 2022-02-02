065ddd9cc54e540316e7f1518d631170650e1ec2
Rahmawati88 <rahmaaditya0109.com>
2022-02-02 04:04:58 -1200

    project1

---
+++ Day1_Welcome_java.java
@@ -0,0 +1,10 @@
+/**
+ * javaClass
+ */
+public class javaClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
